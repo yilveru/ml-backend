842788f2624ba6186d9562e6630e6c636ca4dc3a
Yilver Andres Uran <yuranh@chefcompany.co>
2025-02-20 19:50:38 -0500

    removing debug

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,13 +7,7 @@ import { User } from './users/entities/user.entity';
 import { Product } from './products/entities/product.entity';
 import { ProductsModule } from './products/products.module';
 import { AuthModule } from './auth/auth.module';
-import * as dotenv from 'dotenv';
-dotenv.config();
-console.log({"DB_HOST: ": process.env.DB_HOST,
-"DB_USER: ":process.env.DB_USER,
-"DB_PASS: ":process.env.DB_PASS,
-"DB_NAME: ":process.env.DB_NAME
-});
+
 @Module({
   imports: [
     UsersModule,
