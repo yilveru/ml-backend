897b8852e5de4b40e1f3d8ece50d3be360ee00b0
Yilver Andres Uran <yuranh@chefcompany.co>
2025-02-20 19:32:12 -0500

    get dotenv to load enviroment vars

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,8 @@ import { User } from './users/entities/user.entity';
 import { Product } from './products/entities/product.entity';
 import { ProductsModule } from './products/products.module';
 import { AuthModule } from './auth/auth.module';
+import * as dotenv from 'dotenv';
+dotenv.config();
 console.log({"DB_HOST: ": process.env.DB_HOST,
 "DB_USER: ":process.env.DB_USER,
 "DB_PASS: ":process.env.DB_PASS,
